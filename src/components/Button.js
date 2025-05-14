560557a83b7be884215810c3492e50f52c834db6
JasminHT <JasminHT@users.noreply.github.com>
2025-05-14 11:46:50 -0400

    Allow hover-out as well as hover-in

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,26 @@
-import React, { useRef} from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { useHover } from 'usehooks-ts';
-import { useHoverText } from '../hooks/useHoverText.js'
+import { useSetHoverText, useClearHoverText } from '../hooks/useHoverText.js'
 
 
 export default function Button ({text, disabled, hoverText, onClick }) {
 
-  const setHoverText = useHoverText((state) => state.setText);
+  const setHoverText = useSetHoverText();
+  const clearHoverText = useClearHoverText();
 
   const hoverRef = useRef(null)
   const isHover = useHover(hoverRef)
   
-  if (isHover)
-    setHoverText(hoverText);
+  //detecting mouse hovers and leaves
+  useEffect(() => {
+    if (isHover)
+      setHoverText(hoverText);
+    else
+      clearHoverText();
+  }, [isHover, hoverText] );
 
-
+  //buttons can be disabled
   return (
       <div className='Button'>
         <button ref={hoverRef} disabled={disabled} onClick={onClick} >
--- src/hooks/useHoverText.js
+++ src/hooks/useHoverText.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 
+let clearTimeoutId; //used to debounce hovers
 
 export function useSetHoverText() {
   return useHoverText((state) => state.setText)
@@ -9,11 +10,25 @@ export function useGetHoverText() {
   return useHoverText((state) => state.hoverText)
 }
 
+export function useClearHoverText() {
+  return useHoverText((state) => state.clearText)
+}
+
 export const useHoverText = create(
 
   (set, get) => ({
     hoverText: "no text yet",
-    setText: (text) => set({hoverText: "Cost: "+text} ),
+    setText: (text) => {
+      clearTimeout(clearTimeoutId);
+      set({hoverText: "Cost: "+text});
+    },
+    clearText: () => {
+      clearTimeout(clearTimeoutId);
+      clearTimeoutId = setTimeout(
+        () => set({ hoverText: "" })
+      );
+    }
+
   })
 
 )
